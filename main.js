51a97cfcecb12628eeaca19b71a677e0ccc75e02
Jaakkko <jaakkole2@gmail.com>
2021-05-13 16:54:31 +0300

    Touch screen

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const img = document.getElementById("picture")
 const frame = document.getElementById("frame")
 
+let frameClientX = 0
+let frameClientY = 0
 let frameWidth = 0
 let frameHeight = 0
 
@@ -8,7 +10,10 @@ let scale = 1
 let originX = 0
 let originY = 0
 const transform = {
-  scale(frameX, frameY, imageX, imageY, scaleFactor) {
+  scale(frameX, frameY, scaleFactor) {
+    const imageX = (frameX - originX) / scale + originX
+    const imageY = (frameY - originY) / scale + originY
+
     // update scale
     scale = Math.max(1, scale * scaleFactor)
 
@@ -35,16 +40,19 @@ const transform = {
   }
 }
 
-const frameCoords = (event) => [
-  (event.offsetX - originX) * scale + originX,
-  (event.offsetY - originY) * scale + originY
-]
+const updateFrameCoords = () => {
+  const { left, top } = frame.getBoundingClientRect()
+  frameClientX = left + frame.clientLeft
+  frameClientY = top + frame.clientTop
+}
+
 const limitOrigin = () => {
   originX = Math.max(0, Math.min(frameWidth, originX))
   originY = Math.max(0, Math.min(frameHeight, originY))
 }
 
-img.onload = () => {
+window.onload = () => {
+  updateFrameCoords()
   frameWidth = frame.clientWidth
   frameHeight = frame.clientHeight
 
@@ -52,6 +60,7 @@ img.onload = () => {
   img.style.transform = `scale(${scale})`
 
   window.onresize = () => {
+    updateFrameCoords()
     originX = originX / frameWidth * frame.clientWidth
     originY = originY / frameHeight * frame.clientHeight
     img.style.transformOrigin = `${originX}px ${originY}px`
@@ -59,19 +68,68 @@ img.onload = () => {
     frameHeight = frame.clientHeight
   }
 
-  img.onwheel = event => {
+  frame.onwheel = event => {
     event.preventDefault()
+    const frameX = event.clientX - frameClientX
+    const frameY = event.clientY - frameClientY
+    if (frameX < 0 || frameWidth < frameX || frameY < 0 || frameHeight < frameY) {
+      return
+    }
+
     const scaleFactor = event.deltaY < 0 ? 1.1 : (1 / 1.1)
-    const [frameX, frameY] = frameCoords(event)
-    transform.scale(frameX, frameY, event.offsetX, event.offsetY, scaleFactor)
+    transform.scale(frameX, frameY, scaleFactor)
+  }
+
+  let evCache = new Array()
+  let prevDiff = -1
+  const onMove = event => {
+    for (let i = 0; i < evCache.length; i++) {
+      if (event.pointerId === evCache[i].pointerId) {
+        evCache[i] = event
+        break
+      }
+    }
+
+    switch (evCache.length) {
+      case 1:
+        transform.translate(event.movementX, event.movementY)
+        break
+      case 2:
+        {
+          const x1 = evCache[0].clientX - frameClientX
+          const x2 = evCache[1].clientX - frameClientX
+          const y1 = evCache[0].clientY - frameClientY
+          const y2 = evCache[1].clientY - frameClientY
+
+          const frameX = (x1 + x2) / 2
+          const frameY = (y1 + y2) / 2
+
+          const curDiff = Math.hypot(x1 - x2, y1 - y2)
+          if (prevDiff !== -1) {
+            transform.translate(event.movementX / 2, event.movementY / 2)
+            transform.scale(frameX, frameY, curDiff / prevDiff)
+          }
+          prevDiff = curDiff
+          break
+        }
+    }
   }
 
   img.onpointerdown = (e) => {
-    img.onpointermove = event => transform.translate(event.movementX, event.movementY)
+    prevEvent = e
+    evCache.push(e)
+    img.onpointermove = onMove
     img.setPointerCapture(e.pointerId)
   }
-  img.onpointerup = (e) => {
+  img.onpointerup = (event) => {
+    for (let i = 0; i < evCache.length; i++) {
+      if (event.pointerId === evCache[i].pointerId) {
+        evCache.splice(i, 1)
+        break
+      }
+    }
+    prevDiff = -1
     img.onpointermove = null
-    img.releasePointerCapture(e.pointerId)
+    img.releasePointerCapture(event.pointerId)
   }
 }
